f358f65c20713b8f139a9aeb1870f18901d4f604
NashPL <nash25pl@gmail.com>
2019-07-12 02:17:01 +0100

    Added comments to the file

--- app/Http/Index.php
+++ app/Http/Index.php
@@ -4,9 +4,16 @@ namespace Http;
 include_once(dirname(__DIR__) . '/Lib/User.php');
 use Lib\User;
 
+/**
+ * A Class to handle first set of POST data incoming from index.html form
+ * It creates new User Object, saves the data in database or retrieves it if user email is already in the record. 
+ */
 class Index
 {
     private $user;
+    /**
+     * Constructor handles incoming POST data and build USER Object. 
+     */
     public function __construct($requestData)
     {
         $this->user = new User($requestData['email']);
@@ -19,6 +26,10 @@ class Index
         }
     }
 
+    /**
+     * Function to trigger processUser function so FrontEnd does not do it directly. 
+     * @return String User email. 
+     */
     public function processUser()
     {
         return $this->user->processUser();
