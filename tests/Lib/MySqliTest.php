c467c9c4f570f4e3c76bc0ecac0ad92c964f0976
NashPL <nash25pl@gmail.com>
2019-07-12 02:53:23 +0100

    Fixed test so it uses new names for the files

--- tests/Lib/MySqliTest.php
+++ tests/Lib/MySqliTest.php
@@ -1,28 +1,34 @@
 <?php
 declare(strict_types=1);
 
-include_once(dirname(dirname(__DIR__)) . '/app/Lib/MySqli.php');
+include_once(dirname(dirname(__DIR__)) . '/app/Lib/MysqlConnector.php');
 
 use PHPUnit\Framework\TestCase;
-use Lib\MySqli;
+use Lib\MysqlConnector;
 
 
 final class MySqliTest extends TestCase
 {
     public function testInitializeClass()
     {
-        $mySqli = new MySqli();
+        $mySqli = new MysqlConnector();
         $this->assertTrue(true);
         $mySqli->closeConnection();
     }
 
     public function testSelectQuery()
     {
-        $mySqli = new MySqli();
-        $query = "SELECT * FROM phpDev_user where name like 'Test User 1'";
+        $mySqli = new MysqlConnector();
+        $mysqliObject = $mySqli->getMySQLObject();
+        $query = "SELECT * FROM phpDev_user WHERE name = ?";
+        $name = 'Test User 1';
         $expected = array('id' => 1, 'name' => 'Test User 1', 'password' => 'non encoded password', 'email_address' => 'test@test.com', 'phone_number' => '+44123456789');
-        $result = $mySqli->select($query);
+        $statement = $mysqliObject->prepare($query);
+        $statement->bind_param('s', $name);
+        $result = $mySqli->select($statement);
         $mySqli->closeConnection();
         $this->assertEquals($expected, $result[0]);
     }
+
+
 }
